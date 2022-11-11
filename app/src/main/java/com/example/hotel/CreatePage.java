c7d2b79c7c6cef9985768997725dbee050d4db67
Vip_Ghost <lied00@mail.ru>
2022-11-11 23:12:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hotel/CreatePage.java
@@ -0,0 +1,14 @@
+package com.example.hotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreatePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_page);
+    }
+}
